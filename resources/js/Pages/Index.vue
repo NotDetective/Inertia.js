a82b2870df7d901d1e82ec101eef86f990552665
Micha <micha-elmans@outlook.com>
2023-11-22 15:47:41 +0100

    Update Index.vue
    added links to the lists

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -4,7 +4,7 @@ import {Head} from '@inertiajs/inertia-vue3'
 
 <template>
 
-    <Head title="Demo - Index" />
+    <Head title="Demo - Index"/>
 
     <div class="bg-gray-100 p-4">
         <h1 class="text-2xl font-bold mb-4">Laravel, Vue 3 and Inertia Demo Pages</h1>
@@ -14,21 +14,44 @@ import {Head} from '@inertiajs/inertia-vue3'
         <p class="mb-4">The site is a simple user management system. It has the following pages:</p>
 
         <ul class="list-disc list-inside">
-            <li>Home</li>
-            <li>Users</li>
-            <li>Users/Create</li>
-            <li>Users/Edit</li>
+            <li>
+                <Link :href="route('index')" class="text-blue-500 hover:underline">Home</Link>
+            </li>
+            <li>
+                <Link :href="route('users')" class="text-blue-500 hover:underline">Users</Link>
+            </li>
+            <li>
+                <Link :href="route('users.create')" class="text-blue-500 hover:underline">Users/Create</Link>
+            </li>
+            <li>
+                <Link :href="route('users.edit', 11)" class="text-blue-500 hover:underline">Users/Edit</Link>
+            </li>
+            <li>
+                <Link :href="route('logout')" method="post" as="button" class="text-blue-500 hover:underline">Login</Link>
+            </li>
         </ul>
 
         <p class="mb-4">The site uses the following packages:</p>
 
         <ul class="list-disc list-inside">
-            <li>Laravel 1</li>
-            <li>Vue 3</li>
-            <li>Inertia</li>
-            <li>Tailwind CSS</li>
-            <li>Ziggy</li>
-            <li>Lodash</li>
+            <li>
+                <a href="https://laravel.com/docs/10.x" target="_blank" class="text-blue-500 hover:underline">Laravel 10</a>
+            </li>
+            <li>
+                <a href="https://vuejs.org/" target="_blank" class="text-blue-500 hover:underline">Vue 3</a>
+            </li>
+            <li>
+                <a href="https://inertiajs.com/" target="_blank" class="text-blue-500 hover:underline">Inertia</a>
+            </li>
+            <li>
+                <a href="https://tailwindcss.com/" target="_blank" class="text-blue-500 hover:underline">Tailwind CSS</a>
+            </li>
+            <li>
+                <a href="https://github.com/tighten/ziggy" target="_blank" class="text-blue-500 hover:underline">Ziggy</a>
+            </li>
+            <li>
+                <a href="https://lodash.com/" target="_blank" class="text-blue-500 hover:underline">Lodash</a>
+            </li>
         </ul>
 
         <p class="mb-4">The site uses the following features:</p>
@@ -43,6 +66,10 @@ import {Head} from '@inertiajs/inertia-vue3'
             <li>Form validation</li>
             <li>Form error messages</li>
         </ul>
+
+        <p class="mb-4">
+            Note that the site is mostly tailwind ui components. And make use of dummy data.
+        </p>
 
     </div>
 </template>
