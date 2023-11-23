60c42b9e556ac63cf54340ea9763aef3265e7219
Micha <micha-elmans@outlook.com>
2023-11-23 10:51:07 +0100

    cleaned up the routes
    moved the routes to a controller and request to a request

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\LoginController;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'create'])
@@ -19,67 +18,21 @@ Route::post('/logout', [LoginController::class, 'destroy'])
 Route::middleware(['auth'])
     ->group(function () {
 
-        Route::get('/', function () {
-            return Inertia::render('Index');
-        })->name('index');
+        Route::get('/', [Controller::class, 'indexPage'])
+            ->name('index');
 
-        Route::get('/users', function (Request $request) {
-            $users = User::query()
-                ->select('id', 'name', 'email')
-                ->when($request->input('search'), function ($query, $search) {
-                    $query->where('name', 'LIKE', "%{$search}%");
-                })
-                ->paginate(8)
-                ->withQueryString();
-            return Inertia::render('Users', [
-                'users' => $users,
-                'filters' => $request->all('search'),
-            ]);
-
-        })->name('users');
-
-        Route::get('/users/create', function () {
-            return Inertia::render('Create');
-        })->name('users.create');
-
-        Route::post('/users', function (Request $request) {
-            //added sleep for testing purposes
-            sleep(3);
-
-            $request->validate([
-                'name' => 'required',
-                'email' => 'required|email|unique:users,email',
-            ]);
-            User::create([
-                'name' => $request->input('name'),
-                'email' => $request->input('email'),
-                'password' => 'password',
-            ]);
-            return to_route('users');
-        })->name('users.store');
-
-        Route::get('/users/{user}/edit', function (User $user) {
-            return Inertia::render('Edit', [
-                'user' => $user->only('id', 'name', 'email'),
-            ]);
-        })->name('users.edit');
-
-        Route::patch('/users/{user}/edit', function (Request $request, User $user){
-            $request->validate([
-                'name' => 'required',
-                'email' => 'required|email|unique:users,email,' . $user->id,
-            ]);
-
-            $user->update([
-                'name' => $request->input('name'),
-                'email' => $request->input('email'),
-            ]);
-            return to_route('users');
-        })->name('users.update');
-
-        Route::delete('/users/{user}', function (User $user) {
-            $user->delete();
-            return to_route('users');
-        })->name('users.destroy');
+        // Users
+        Route::get('/users', [UserController::class, 'index'])
+            ->name('users');
+        Route::get('/users/create', [UserController::class, 'create'])
+            ->name('users.create');
+        Route::post('/users', [UserController::class, 'store'])
+            ->name('users.store');
+        Route::get('/users/{user}/edit', [UserController::class, 'edit'])
+            ->name('users.edit');
+        Route::patch('/users/{user}/edit', [UserController::class, 'update'])
+            ->name('users.update');
+        Route::delete('/users/{user}', [UserController::class, 'destroy'])
+            ->name('users.destroy');
 
     });
